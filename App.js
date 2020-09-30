d642fd6f38a000868a28b780dffada101a12a36a
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-09-30 01:57:21 -0600

    Chat screen finished - App ended

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, LogBox } from 'react-native';
 import { Container } from 'native-base';
 import Login from './src/screens/Login';
 import Chat from './src/screens/Chat';
+
+LogBox.ignoreLogs(['Setting a timer']);
 
 export default function App() {
   const [userName, setUserName] = useState(null);
 
   return (
     <Container style={styles.Container}>
-      {!userName ? <Login setUserName={setUserName} /> : <Chat />}
+      {!userName ? (
+        <Login setUserName={setUserName} />
+      ) : (
+        <Chat userName={userName} />
+      )}
     </Container>
   );
 }
--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Header, Body, Title } from 'native-base';
+import moment from 'moment';
+import { map } from 'lodash';
+import Input from '../components/Input';
+import Message from '../components/Message';
+import firebase from '../utils/firebase';
+import 'firebase/database';
 
-export default function Chat() {
+export default function Chat(props) {
+  const { userName } = props;
+  const [messages, setMessages] = useState([]);
+  const chatScrollRef = useRef();
+
+  useEffect(() => {
+    const chat = firebase.database().ref('general');
+    chat.on('value', (snapshot) => {
+      setMessages(snapshot.val());
+    });
+  }, []);
+
+  useEffect(() => {
+    chatScrollRef.current.scrollToEnd({ animated: true });
+  }, [messages]);
+
+  const sendMessage = (message) => {
+    const time = moment().format('hh:mm a');
+    firebase.database().ref('general').push({ userName, text: message, time });
+  };
+
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
+    <>
+      <Header style={styles.header} iosBarStyle="light-content">
+        <Body>
+          <Title style={styles.headerText}>Chateappme</Title>
+        </Body>
+      </Header>
+      <View style={styles.content}>
+        <ScrollView style={styles.chatView} ref={chatScrollRef}>
+          {map(messages, (message, index) => (
+            <Message key={index} message={message} myName={userName} />
+          ))}
+        </ScrollView>
+        <Input sendMessage={sendMessage} />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  header: {
+    backgroundColor: '#16202b',
+  },
+  headerText: {
+    color: '#fff',
+  },
+  chatView: {
+    backgroundColor: '#1b2734',
+  },
+});
